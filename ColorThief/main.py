0e228bb39b5a9508f37f71ff7278ff87f5b6228c
Debasis Dakua <102685615+Debasis-Dakua@users.noreply.github.com>
2024-04-11 21:25:20 +0530

    Update main.py

--- ColorThief/main.py
+++ ColorThief/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 from sklearn.cluster import KMeans
 
+
 #function for dominant color
 def get_dominant_colors(img_path,num_colors=1):
     img = cv2.imread(img_path)
@@ -26,6 +27,24 @@ def get_all_colors(img_path,num_color):
 
     cluster_centers = kmeans.cluster_centers_.astype(int)
     return cluster_centers
+
+#function for colorpercentage
+def calculate_color_percentage(img_path, colors,decimal_places=2):
+
+    img = cv2.imread(img_path) #read the img
+    total_pixels = img.shape[0]*img.shape[1] # total number of pixel
+    pixels = img.reshape(-1,3)
+    counts = np.zeros(len(colors),dtype=int)
+
+    for pixel in pixels:
+        nearest_color_index = np.argmin(np.linalg.norm(colors - pixel, axis=1))
+        counts[nearest_color_index] += 1
+
+    percentages = (counts / total_pixels) * 100
+    rounded_percentages = np.round(percentages, decimal_places)
+    percentage_strings = [f"{rounded:.{decimal_places}f}%" for rounded in rounded_percentages] # for concat % symbol
+    return percentage_strings
+
 
 def display_colors(colors):
     height =200
@@ -55,11 +74,13 @@ def display_all_colors(colors):
     cv2.destroyAllWindows()
 
 
+
+
 if __name__=="__main__":
-    img_path = "sample.jpeg"  #image path
+    img_path = "google.jpeg"  #image path
 
     num_colors = 1   #number of dominant color
-    num_color=9      #total number of color present to find
+    num_color=5     #total number of color present to find
 
     dominant_colors = get_dominant_colors(img_path, num_colors) #calling the fun
     display_colors(dominant_colors) #displying it
@@ -67,6 +88,9 @@ if __name__=="__main__":
     all_colors = get_all_colors(img_path,num_color)
     display_all_colors(all_colors)
 
+    color_percentages = calculate_color_percentage(img_path,all_colors, decimal_places=2)
+
     print("Dominant color:",dominant_colors)
     print("All colors RGB format:",all_colors)
+    print("color percentage",color_percentages)
 
